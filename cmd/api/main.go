6989da181257fc2259a816cb6dbc5c52bc4c94f4
Ishan Shrestha <ishan.shrestha356@gmail.com>
2023-07-13 07:27:19 +0545

    feat: successfull configuring connecting to database

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,10 +4,13 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/ishanshre/GoFiberRestApiJWTAuth/internals/config"
+	"github.com/ishanshre/GoFiberRestApiJWTAuth/internals/drivers"
 	"github.com/ishanshre/GoFiberRestApiJWTAuth/internals/routers"
+	"github.com/joho/godotenv"
 )
 
 var global config.AppConfig
@@ -15,10 +18,48 @@ var infoLog *log.Logger
 var errorLog *log.Logger
 
 func main() {
+	// Configire flag and parse it.
 	flag.IntVar(&global.Port, "port", 8000, "Port that servert listen to")
 	flag.StringVar(&global.DbString, "dbString", "postgres", "Database string name")
 	flag.Parse()
+
+	// global config
+	global.InProduction = false
+
+	infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime)
+
+	global.InfoLog = infoLog
+	global.ErrorLog = errorLog
+
+	db, err := run()
+	if err != nil {
+		global.ErrorLog.Println(err)
+	}
+
+	defer db.SQL.Close()
+
+	// create a new fiber app
 	app := fiber.New()
+
+	// pass fiber app to router to create routes
 	routers.Router(&global, app)
+
+	// start the server
 	app.Listen(fmt.Sprintf(":%d", global.Port))
+}
+
+func run() (*drivers.DB, error) {
+
+	// load environement files
+	if err := godotenv.Load(".env"); err != nil {
+		return nil, fmt.Errorf("error in loading the environment files: %v", err.Error())
+	}
+
+	// connect to database
+	db, err := drivers.ConnectSql(global.DbString, os.Getenv(global.DbString))
+	if err != nil {
+		return nil, fmt.Errorf("error in connecting to database: %v", err.Error())
+	}
+	return db, nil
 }
--- internals/drivers/driver.go
+++ internals/drivers/driver.go
@@ -1,1 +1,54 @@
 package drivers
+
+import (
+	"database/sql"
+	"time"
+
+	_ "github.com/lib/pq"
+)
+
+type DB struct {
+	SQL *sql.DB
+}
+
+// dbConn holds the db global instance
+var dbConn = &DB{}
+
+const (
+	maxOpenDBConn = 10
+	maxIdelDBConn = 5
+	maxLifeDBTime = 5 * time.Minute
+)
+
+func ConnectSql(dbString, dsn string) (*DB, error) {
+	d, err := newDatabase(dbString, dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	d.SetMaxIdleConns(maxIdelDBConn)
+	d.SetMaxOpenConns(maxOpenDBConn)
+	d.SetConnMaxLifetime(maxLifeDBTime)
+
+	dbConn.SQL = d
+
+	if err := testDB(d); err != nil {
+		return nil, err
+	}
+	return dbConn, nil
+}
+
+func newDatabase(dbString, dsn string) (*sql.DB, error) {
+	db, err := sql.Open(dbString, dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+func testDB(d *sql.DB) error {
+	return d.Ping()
+}
